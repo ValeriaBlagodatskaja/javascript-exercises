ef7d82ee56fb2284c235bc18cbb1733bd1212545
Valeria48562 <vblagodatskaja@gmail.com>
2023-06-12 22:10:39 +0300

    main

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldestPerson = people.reduce((oldest, current) => {
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const currentAge = current.yearOfDeath - current.yearOfBirth;
+    if (currentAge > oldestAge) {
+      return current;
+    } else {
+      return oldest;
+    }
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
