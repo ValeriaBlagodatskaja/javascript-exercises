02d3004eb24bd97cb67218341a5f2d6cc514dd90
Valeria48562 <vblagodatskaja@gmail.com>
2023-06-12 14:12:03 +0300

    main

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  } else if (typeof n === "string" && Number(n) <= 1) {
+    return Number(n);
+  } else if (typeof n === "string") {
+    return fibonacci(Number(n) - 1) + fibonacci(Number(n) - 2);
+  } else if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
 
 // Do not edit below this line
